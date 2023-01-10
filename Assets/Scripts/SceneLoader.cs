5a944dba4c9ea4de11b9077bf479821f7d66a73d
Sebastian Alpizar <sebas_alpizar@hotmail.com>
2023-01-10 16:58:19 -0600

    Added changes for sprites

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,12 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
-{
+public class SceneLoader : MonoBehaviour {
+    
+    [SerializeField]
+    private Object targetScene;
+    
     public static void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void OnTriggerEnter2D(Collider2D col) {
+        if (col.CompareTag("Player")) {
+            SceneManager.LoadScene(targetScene.name);
+        }
     }
 }
