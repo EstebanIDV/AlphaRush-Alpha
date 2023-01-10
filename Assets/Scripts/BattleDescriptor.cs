83c49c1bef21b02883bab7dceba995da64d31344
Esteban Durán <eiduran@estudiantec.cr>
2023-01-10 16:39:41 -0600

    Pass Enemies as arguments for battles implemented

---
+++ Assets/Scripts/BattleDescriptor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleDescriptor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public GameObject Enemy1;
+    public GameObject Enemy2;
+    public GameObject Enemy3;
+    
+}
